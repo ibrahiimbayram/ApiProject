f4f1211bf2a209aa7303a5e1d4f3ea21c794eac4
İbrahim Bayram <47785669+ibrahiimbayram@users.noreply.github.com>
2021-03-06 01:14:24 +0300

    DataAcces layer added

---
+++ DataAcces/Context/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Entity.Tables;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcces.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
