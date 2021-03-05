c38b13b201a773d5b1f7cdb43cc171548adf1f03
İbrahim Bayram <47785669+ibrahiimbayram@users.noreply.github.com>
2021-03-06 02:29:47 +0300

    Generic Interfaces and Generic Repository added

---
+++ DataAcces/Concrete/EfGenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using DataAcces.Context;
+using DataAcces.Interfaces;
+using Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcces.Concrete
+{
+    public class EfGenericRepository<TEntity> : IGenericDal<TEntity> where TEntity : class, IEntity, new()
+    {
+        private readonly DataContext _context;
+        public EfGenericRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ DataAcces/Interfaces/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcces.Interfaces
+{
+    public interface IGenericDal<TEntity> where TEntity : class ,IEntity,new()
+    {
+        Task<List<TEntity>> GetAllAsync();
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+    }
+}
