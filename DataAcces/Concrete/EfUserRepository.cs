a0da027106220ba1eaad00d49b151e8c33bc8098
İbrahim Bayram <47785669+ibrahiimbayram@users.noreply.github.com>
2021-03-13 08:01:17 +0300

    UserRepository Added

---
+++ DataAcces/Concrete/EfUserRepository.cs
@@ -0,0 +1,24 @@
+﻿using DataAcces.Context;
+using DataAcces.Interfaces;
+using Entity.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcces.Concrete
+{
+    public class EfUserRepository : EfGenericRepository<Users> , IUserDal
+    {
+
+        private readonly DataContext _context;
+
+        public EfUserRepository(DataContext context):base(context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
